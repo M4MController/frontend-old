8d7b60449b5c69d275530a51facbe4aed00887ce
Yaxo-O <aaazzzx@mail.ru>
2018-04-30 20:21:37 +0300

    Submit trouble

---
+++ src/components/ControllerWindow/index.js
@@ -0,0 +1,49 @@
+/**
+ * Created by YAX on 30/04/2018.
+ */
+
+'use strict';
+
+import React from 'react';
+import ModularWindow from '../ModularWindow/index'
+
+import 'index.scss'
+
+export default class ControllerWindow extends React.Component{
+  constructor(props){
+    super(props);
+
+    this.state = {address: "00:00:00:00:00"};
+    this.submitAction = props.submitAction;
+    this.submit = this.submit.bind(this);
+  }
+
+  submit(event){
+    this.submitAction();
+    alert("!");
+    console.log("!");
+    event.preventDefault();
+  }
+
+
+  render(){
+    const wid = '350px';
+    const hei = '110px';
+
+    return (
+      <ModularWindow width={wid} height={hei}>
+        <div className='controller-head'>
+          Controller
+        </div>
+        <div className='controller-body'>
+          <form onSubmit={this.submit}>
+            <label>Mac address: </label>
+            <input type='text'
+              pattern='[0-9a-fA-f]{2}:[0-9a-fA-f]{2}:[0-9a-fA-f]{2}:[0-9a-fA-f]{2}:[0-9a-fA-f]{2}:[0-9a-fA-f]{2}'/>
+              <input type='submit' value='Enter' className='controller-button'/>
+          </form>
+        </div>
+      </ModularWindow>
+    )
+  }
+}
---
+++ src/components/ModularWindow/index.js
@@ -0,0 +1,26 @@
+/**
+ * Created by YAX on 29/04/2018.
+ */
+
+'use strict';
+
+import React from 'react';
+
+import 'index.scss';
+
+export default class ModularWindow extends React.Component{
+  render(){
+    const wid = this.props.width || 200;
+    const hei = this.props.height || 100;
+    return (
+      <div className='blocking-object'>
+        Some text goes here
+        <div className='modular' style={{width: wid, height: hei}}>
+          <body>
+            {this.props.children}
+          </body>
+        </div>
+      </div>
+    )
+  }
+}
