b8b1d95aed7748515213d2e9310e43d5df52dc98
Yaxo-O <aaazzzx@mail.ru>
2018-05-26 17:27:54 +0300

    props fixed

--- src/components/user-controll/Pop-Up-Menu/index.js
+++ src/components/user-controll/Pop-Up-Menu/index.js
@@ -5,12 +5,29 @@ import React from 'react';
 import 'index.scss';
 
 export default class extends React.Component{
+  constructor(props) {
+    super(props);
+  }
+
   render() {
+
+    let elements = this.props.elements || [];
+    console.log(elements);
 
     return (
       <div className='menu-container'>
-        Some text
+        {
+          elements.map((elem) => <div onClick={elem.onClick}>{elem.text}</div>)
+        }
       </div>
     )
   }
 }
+
+/*
+ elements.forEach((elem) => {
+ <div onClick={elem.onClick}>
+ {elem.text}
+ </div>
+ })
+ */
--- src/components/user-controll/index.js
+++ src/components/user-controll/index.js
@@ -13,21 +13,30 @@ export default class extends React.Component {
       this.getMenu = this.getMenu.bind(this);
       this.toggle = this.toggle.bind(this);
       this.state = {showMenu: false};
+      this.hideMenu = this.hideMenu.bind(this);
+
+      this.menuElems = [{text: "Log out", onClick: this.hideMenu},];
   }
 
   toggle() {
     this.setState({showMenu: !this.state.showMenu});
   }
 
+  hideMenu() {
+    this.setState({showMenu: false});
+  }
+
   getMenu() {
     if(this.state.showMenu)
-      return (<Menu className="visible"/>);
+      return (<Menu elements={this.menuElems} className="visible"/>);
 
     return (<noscript/>);
   }
   render() {
     let img = 'public/no-image.jpg';
     let username = 'Unnamed';
+    if(this.props.user)
+      img = (this.props.user.secondName + this.props.user.familyName) || username;
     return (
       <div className='container'>
         <img src={img} className='avatar'/>
