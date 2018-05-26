34a9908b6a206f2d5577c8ece51970dfaf90dee0
Yaxo-O <aaazzzx@mail.ru>
2018-05-26 14:00:11 +0300

    Pop-up menu fixed

---
+++ src/components/user-controll/Pop-Up-Menu/index.js
@@ -0,0 +1,16 @@
+'use strict';
+
+import React from 'react';
+
+import 'index.scss';
+
+export default class extends React.Component{
+  render() {
+
+    return (
+      <div className='menu-container'>
+        Some text
+      </div>
+    )
+  }
+}
--- src/components/user-controll/index.js
+++ src/components/user-controll/index.js
@@ -2,9 +2,29 @@
 
 import React from 'react';
 
+import Menu from './Pop-Up-Menu';
+
 import 'index.scss';
 
 export default class extends React.Component {
+  constructor(props) {
+      super(props);
+
+      this.getMenu = this.getMenu.bind(this);
+      this.toggle = this.toggle.bind(this);
+      this.state = {showMenu: false};
+  }
+
+  toggle() {
+    this.setState({showMenu: !this.state.showMenu});
+  }
+
+  getMenu() {
+    if(this.state.showMenu)
+      return (<Menu className="visible"/>);
+
+    return (<noscript/>);
+  }
   render() {
     let img = 'public/no-image.jpg';
     let username = 'Unnamed';
@@ -12,6 +32,8 @@ export default class extends React.Component {
       <div className='container'>
         <img src={img} className='avatar'/>
         <span className='username'>{username}</span>
+        <span className='username' onClick={this.toggle}>X</span>
+        {this.getMenu()}
       </div>
     );
   }
